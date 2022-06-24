53bbcd6018e78c6387fdf6dc8ff982a3d178f213
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-24 23:18:44 +0530

    Added: Even or Odd Programme

--- src/com/coreprograms/CoreAndFunctional.java
+++ src/com/coreprograms/CoreAndFunctional.java
@@ -24,4 +24,16 @@ public class CoreAndFunctional {
         System.out.println("count of Tails = " + tails);
 
     }
+
+    public static void  evenOrOdd(){
+        int number;
+        System.out.println("Enter the Number : ");
+        Scanner value = new Scanner(System.in);
+        number = value.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Entered Number is Even");
+        } else {
+            System.out.println("Entered Number is Odd");
+        }
+    }
 }
--- src/com/coreprograms/Main.java
+++ src/com/coreprograms/Main.java
@@ -1,11 +1,27 @@
 package com.coreprograms;
 
+import java.util.Scanner;
+
 import static com.coreprograms.CoreAndFunctional.coinFlip;
+import static com.coreprograms.CoreAndFunctional.evenOrOdd;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome To Core And Functional Programs");
-        coinFlip();
+        System.out.println("Please Enter Your Choice ");
+        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme");
+
+        Scanner scanner = new Scanner(System.in);
+        int option =scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                coinFlip();
+                break;
+            case 2:
+                evenOrOdd();
+                break;
+        }
     }
 }
