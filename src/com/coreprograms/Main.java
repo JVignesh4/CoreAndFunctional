bdc86b076dae238e5b24b2c8847bd2b3df9795f2
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-24 23:22:09 +0530

    Added: Largest Number Programme

--- src/com/coreprograms/Main.java
+++ src/com/coreprograms/Main.java
@@ -2,15 +2,14 @@ package com.coreprograms;
 
 import java.util.Scanner;
 
-import static com.coreprograms.CoreAndFunctional.coinFlip;
-import static com.coreprograms.CoreAndFunctional.evenOrOdd;
+import static com.coreprograms.CoreAndFunctional.*;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome To Core And Functional Programs");
         System.out.println("Please Enter Your Choice ");
-        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme");
+        System.out.println("1.CoinFlip Programme \n 2.Even Or Odd Programme \n 3.Largest Number programme");
 
         Scanner scanner = new Scanner(System.in);
         int option =scanner.nextInt();
@@ -22,6 +21,11 @@ public class Main {
             case 2:
                 evenOrOdd();
                 break;
+            case 3:
+                largestNumber();
+                break;
+            default:
+                System.out.println("Please Enter Correct option");
         }
     }
 }
